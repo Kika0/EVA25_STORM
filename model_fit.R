0aed1c74cbcd02491d28324e2986d44ec97743df
Kristina Bratkova <91739909+Kika0@users.noreply.github.com>
2025-05-27 15:57:26 +0100

    Update model_fit.R

--- model_fit.R
+++ model_fit.R
@@ -12,46 +12,33 @@ source('conditional_model_helpers.R')
 source('conditional_model_fit.R')
 source('target_quantity_helpers.R')
 source('model_diagnostics.R')
+source('Block_Bootstrapping.R')
 
-### loading data file
-load('Yrun.RData')
-Yrun1Lap <- as.data.frame((Yrun1 %>% apply(c(2),FUN=row_number))/(nrow(Yrun1)+1)) %>% apply(c(1,2),FUN=unif_laplace_pit) %>% as.data.frame()
-
-# marginal and conditional fits and data -----
-load('cond_modelvc.RData')
 
 # obtain bootstrap using model refitting -----
 model_refit <- function(k) {
-  Yboot <- sim_cond_model(Yrun=Yrun1,cond_model=cond_modelvc1,res_dist = "AGG_vinecopula")
-  Ybootemp <- sim_cond_model(Yrun=Yrun1,cond_model=cond_modelvc1,res_dist = "empirical")
+Yboot <- block_bootstrap(i)
 
   cond_model_fit_wrapper = function(i){
     return(fn(site_index=i,v=q,Yrun=Yboot,res_dist = "AGG_vinecopula"))
   }
   cond_modelvc1 <- sapply(1:25,cond_model_fit_wrapper,simplify=FALSE)
-
-cond_model_fit_wrapper = function(i){
-    return(fn(site_index=i,v=q,Yrun=Ybootemp,res_dist = "empirical"))
-  }
-  cond_modelvc1e <- sapply(1:25,cond_model_fit_wrapper,simplify=FALSE)
   
   Nrun <- 50
-   boot <- replicate(n = Nrun, expr = sim_cond_model(Yrun=Yboot,cond_model=cond_modelvc1,res_dist = "AGG_vinecopula"), simplify = FALSE)
-
-  bootemp <- replicate(n = Nrun, expr = sim_cond_model(Yrun=Ybootemp,cond_model=cond_modelvc1e,res_dist = "empirical"), simplify = FALSE)
-
+  # simulate from cond. model with AGG_vinecopula residuals
+  boot <- replicate(n = Nrun, expr = sim_cond_model(Yrun=Yboot,cond_model=cond_modelvc1,res_dist = "AGG_vinecopula"), simplify = FALSE)
   # evaluate target quantities
-
   tq <- do.call(rbind,lapply(bootemp,FUN = Qeval))
   # sum and divide by Nrun
   temp <- apply(tq,MARGIN=2,FUN=sum)/Nrun
-  
+
+  # simulate from cond.model with empirical residuals
+  bootemp <- replicate(n = Nrun, expr = sim_cond_model(Yrun=Ybootemp,cond_model=cond_modelvc1e,res_dist = "empirical"), simplify = FALSE)
   # evaluate target quantities
   tq <- do.call(rbind,lapply(boot,FUN = Qeval))
   # sum and divide by Nrun
   tvc <- apply(tq,MARGIN=2,FUN=sum)/Nrun
 
-  
   # print in latex table
   tq_table <- cbind(data.frame("Residual method" = c("empirical","vine copula")),
                     rbind(temp,tvc))
@@ -64,10 +51,10 @@ cond_model_fit_wrapper = function(i){
 }
 
 d = 25
-q = 0.95
-Nrun = 100
+q = 0.95 # quantile threshold for fitting the conditional model
+Nboot = 100
 # model_refit(1)
-param_bootstrap = mclapply(1:Nrun, model_refit, mc.cores = 100)
+param_bootstrap = mclapply(1:Nboot, model_refit, mc.cores = 100)
 
 # save results -----
 save(param_bootstrap, file = "param_bootstrap3.RData")
