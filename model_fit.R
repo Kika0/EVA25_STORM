e610bfc9f7b7d2a6ebd2f523a9d214e07b6c70e4
Kristina Bratkova <91739909+Kika0@users.noreply.github.com>
2025-05-27 16:52:57 +0100

    Update model_fit.R

--- model_fit.R
+++ model_fit.R
@@ -13,12 +13,20 @@ source('conditional_model_fit.R')
 source('target_quantity_helpers.R')
 source('model_diagnostics.R')
 source('Block_Bootstrapping.R')
+source("NonSta_GPD_Functions.R")
+source("NonSta_GPD_Fitting.R")
+
+# load data -----
+load("runs/run1.RData")
+load("runs/run2.RData")
+load("runs/run3.RData")
+load("runs/run4.RData")
 
 
 # obtain bootstrap using model refitting -----
 model_refit <- function(k) {
-Yboot <- block_bootstrap(i)
-
+source("Block_Bootstrapping.R")
+Yboot <- Y_Bootstrapped[[1]]
   cond_model_fit_wrapper = function(i){
     return(fn(site_index=i,v=q,Yrun=Yboot,res_dist = "AGG_vinecopula"))
   }
@@ -27,14 +35,14 @@ Yboot <- block_bootstrap(i)
   Nrun <- 50
   # simulate from cond. model with AGG_vinecopula residuals
   boot <- replicate(n = Nrun, expr = sim_cond_model(Yrun=Yboot,cond_model=cond_modelvc1,res_dist = "AGG_vinecopula"), simplify = FALSE)
-  # evaluate target quantities
+  # evaluate target quantities for all 6 questions (preliminary+target)
   tq <- do.call(rbind,lapply(bootemp,FUN = Qeval))
   # sum and divide by Nrun
   temp <- apply(tq,MARGIN=2,FUN=sum)/Nrun
 
   # simulate from cond.model with empirical residuals
   bootemp <- replicate(n = Nrun, expr = sim_cond_model(Yrun=Ybootemp,cond_model=cond_modelvc1e,res_dist = "empirical"), simplify = FALSE)
-  # evaluate target quantities
+  # evaluate target quantities for all 6 questions (preliminary+target)
   tq <- do.call(rbind,lapply(boot,FUN = Qeval))
   # sum and divide by Nrun
   tvc <- apply(tq,MARGIN=2,FUN=sum)/Nrun
